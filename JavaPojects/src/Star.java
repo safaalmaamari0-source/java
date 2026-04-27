91b1bdf444c607b2b90ce0820a596d2b5af2e694
safaalmaamari0-source <you@example.com>
2026-04-27 15:24:16 +0400

    star

--- JavaPojects/src/Star.java
+++ JavaPojects/src/Star.java
@@ -1,4 +1,18 @@
 package src;
 
 public class Star {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for (int s = 4; s >= i - 1; s--){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i -1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+
+        }
+    }
 }
